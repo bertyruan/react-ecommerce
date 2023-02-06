dae5e4af04afe84ab2456d185cc3bf765bf19acf
Albert Ruan <berty.ruan@gmail.com>
2023-02-06 17:03:18 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const url = "https://cdn.fs.teachablecdn.com/jXxMUj86Qf2pChV37EzI"
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(url).then(data => data.json()).then(setItems)
+  }, [])
+
+
   return (
     <div>
-      Hello World
+      { items.map(item => item.title) }
     </div>
   )
 }
